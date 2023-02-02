c3cf84f7960549ccfcf61969800af6216e1ef1e1
lsclgr <calegariluisa@gmail.com>
2023-02-02 07:34:55 -0300

    publi sem foto

---
+++ app/src/main/java/br/com/alura/orgs/model/Produto.kt
@@ -0,0 +1,7 @@
+package br.com.alura.orgs.model
+
+data class Produto(
+    val nome: String,
+    val descricao: String,
+    val local: String
+)
---
+++ app/src/main/java/br/com/alura/orgs/ui/activity/FormularioProdutoActivity.kt
@@ -0,0 +1,41 @@
+package br.com.alura.orgs.ui.activity
+
+import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import androidx.appcompat.app.AppCompatActivity
+import br.com.alura.orgs.R
+import br.com.alura.orgs.dao.ProdutosDao
+import br.com.alura.orgs.model.Produto
+
+class FormularioProdutoActivity : AppCompatActivity(R.layout.activity_formulario_produto) {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        configuraBotaoSalvar()
+    }
+
+    private fun configuraBotaoSalvar() {
+        val dao = ProdutosDao()
+        val btnsalvar = findViewById<Button>(R.id.activity_formulario_produto_btnsalvar)
+        btnsalvar.setOnClickListener {
+            val produtoNovo = criaProduto()
+            dao.adiciona(produtoNovo)
+            finish()
+        }
+    }
+
+    private fun criaProduto(): Produto {
+        val campoNome = findViewById<EditText>(R.id.activity_formulario_produto_nome)
+        val nome = campoNome.text.toString()
+        val campoDescricao = findViewById<EditText>(R.id.activity_formulario_produto_descricao)
+        val descricao = campoDescricao.text.toString()
+        val campoLocal = findViewById<EditText>(R.id.activity_formulario_produto_local)
+        val local = campoLocal.text.toString()
+        return Produto(
+            nome = nome,
+            descricao = descricao,
+            local = local
+        )
+    }
+
+}
